f52792bcfb52378c8f23c4984da28689cb1d7f21
7maxabramovich <7maxabramovich@gmail.com>
2021-05-31 16:37:50 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Route1 from './Route1';
 import React, { useState } from 'react';
 import Moment from 'react-moment';
--- src/Route1.js
+++ src/Route1.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import {Link, Switch, Route, BrowserRouter as Router} from "react-router-dom";
 
